0447535405f017eb1058b4a3eee1d59caa080321
cshafer2 <shafer.charlie@gmail.com>
2017-09-04 19:05:41 -0400

    Assignment2
    Intro to GitHub

--- ListFC.py
+++ ListFC.py
@@ -1,6 +1,6 @@
-# Author:  
-# Date:
-# Purpose:  
+# Author:  Charlie Shafer
+# Date:  9/4/17
+# Purpose:  Assignment 2
 
 
 # Import ArcPy
@@ -8,14 +8,15 @@ import arcpy
  
 
 # Set the workspace
- 
+arcpy.env.workspace = "C:\Users\Charlie\Google Drive\MyDocuments\AAGradSchool\Programming in GIS\Assignment2\Newark.gdb"
 
 # List all of the feature classes in the Newark geodatabase 
-
+fclass = arcpy.ListFeatureClasses()
 
 # Iterate through all the feature classes and print the name of each feature class 
-
+for feature in fclass:
+ print feature
 
 # Print "Complete"
-
+print "Complete"
 
