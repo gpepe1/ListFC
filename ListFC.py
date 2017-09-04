b75fb89d7491f3e0fda5326b53f16dabe3cf2365
jalo16 <31590030+jalo16@users.noreply.github.com>
2017-09-04 08:14:17 -0400

    This is a Python script that prints all the feature classes within the Newark file geodatabase.
    I added the appropriate code to print all the features classes within the Newark file geodatabase.

--- ListFC.py
+++ ListFC.py
@@ -1,5 +1,5 @@
-# Author:  
-# Date:
+# Author:  Jason Lowe
+# Date:  September 4, 2017
 # Purpose:  
 
 
@@ -8,14 +8,18 @@ import arcpy
  
 
 # Set the workspace
+arcpy.env.workspace = r"C:\Users\Jason\Desktop\School\606 Information\Module 2\Data for Assignment 2\Assignment2\Newark.gdb"
  
 
-# List all of the feature classes in the Newark geodatabase 
+# List all of the feature classes in the Newark geodatabase
+# fcList is a Python list returned from the ListWorkspaces function 
+fcList = arcpy.ListFeatureClasses()
 
 
 # Iterate through all the feature classes and print the name of each feature class 
-
+for fc in fcList:
+	print fc
 
 # Print "Complete"
-
+print "Complete"
 
