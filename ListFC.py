71cccc2fbe57f817d0dd24d3454076b2a6067242
BoermaJ <joshua.boerma@gmail.com>
2017-09-06 16:31:31 -0400

    Modified file for Assignment 2
    Input the requested elements contirbuting to the homework for Assignement 2 in the Programming in GIS course.

--- ListFC.py
+++ ListFC.py
@@ -1,6 +1,6 @@
-# Author:  
-# Date:
-# Purpose:  
+# Author:  Joshua Boerma
+# Date: 6 Sep 2017
+# Purpose:  For assignment 2 in Programming in GIS
 
 
 # Import ArcPy
@@ -8,14 +8,16 @@ import arcpy
  
 
 # Set the workspace
- 
+ arcpy.env.workspace = r'E:\GIS\School\3Programming\Assignment2(1)\Assignment2\Newark.gdb'
 
 # List all of the feature classes in the Newark geodatabase 
-
+arcpy.ListFeatureClasses()
 
 # Iterate through all the feature classes and print the name of each feature class 
-
+features = arcpy.ListFeatureClasses()
+for feat in features:
+    print feat
 
 # Print "Complete"
-
+print "Complete"
 
