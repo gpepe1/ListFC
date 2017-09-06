73664db0a13afc9552f9f49d4396baffe32fe5b5
pklingm1 <31711800+pklingm1@users.noreply.github.com>
2017-09-06 16:03:31 -0700

    Assignment 2 Code
    List of Feature Classes in Newark GDB.

--- ListFC.py
+++ ListFC.py
@@ -1,6 +1,6 @@
-# Author:  
-# Date:
-# Purpose:  
+# Author:  Peter Klingman
+# Date: 09/06/2017
+# Purpose:  Practice with GitHub
 
 
 # Import ArcPy
@@ -8,14 +8,16 @@ import arcpy
  
 
 # Set the workspace
- 
+arcpy.env.workspace = "c:\\Assignments\\Programming_in_GIS_Fall_17\\Assignment2\\Newark.gdb"
 
 # List all of the feature classes in the Newark geodatabase 
-
+feature_classes = arcpy.ListFeatureClasses()
 
 # Iterate through all the feature classes and print the name of each feature class 
-
+for feature_class in feature_classes:
+    print feature_class
 
 # Print "Complete"
+print "Complete"
 
 
