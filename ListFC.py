5c9fb346e980c32ee1698f14641499dc20ed4698
jmjones0784 <jamesmjones84@yahoo.com>
2017-09-09 13:57:45 -0400

    Complete Script
    Completed script.

--- ListFC.py
+++ ListFC.py
@@ -5,17 +5,18 @@
 
 # Import ArcPy
 import arcpy
- 
+from arcpy import env
+
 
 # Set the workspace
- 
+env.workspace = r'C:\Student\AS.430.606\Assignment2\Assignment2\Newark.gdb'
 
-# List all of the feature classes in the Newark geodatabase 
+# List all of the feature classes in the Newark geodatabase
+fc = arcpy.ListFeatureClasses("*")
 
-
-# Iterate through all the feature classes and print the name of each feature class 
-
+# Iterate through all the feature classes and print the name of each feature class
+for f in fc:
+    print(f)
 
 # Print "Complete"
-
-
+print("Complete")
