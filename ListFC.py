e3bb1d46c242fa2a53091ce459c8a9cba2d30260
cliving4 <31830632+cliving4@users.noreply.github.com>
2017-09-10 17:32:03 -0400

    Assignment 2 Exercise
    Code for Assignment 2, Part III completed.

--- ListFC.py
+++ ListFC.py
@@ -1,6 +1,6 @@
-# Author:  
-# Date:
-# Purpose:  
+# Author: Colleen Livingstone  
+# Date: 9/2017 
+# Purpose: Assignment 2  
 
 
 # Import ArcPy
@@ -8,14 +8,19 @@ import arcpy
  
 
 # Set the workspace
+arcpy.env.workspace = r"C:\Users\Colleen\Documents\GRAD GIS\Programming\Data\Assignment2\Newark.gdb"
  
 
-# List all of the feature classes in the Newark geodatabase 
+# List all of the feature classes in the Newark geodatabase
+fcList = arcpy.ListFeatureClasses()
 
 
-# Iterate through all the feature classes and print the name of each feature class 
+# Iterate through all the feature classes and print the name of each feature class
+for fc in fcList:
+    print (fc)
 
 
 # Print "Complete"
+print ("Complete")
 
 
