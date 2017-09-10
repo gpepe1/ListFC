c9b15236039577ad720b5c4da892f08c92a6d09a
CRehak1 <31809927+CRehak1@users.noreply.github.com>
2017-09-09 18:49:16 -0600

    ListFC Commit1
    Lesson 2, ListFC GitHub Commit1

--- ListFC.py
+++ ListFC.py
@@ -1,6 +1,6 @@
-# Author:  
-# Date:
-# Purpose:  
+# Author:  Chris Rehak
+# Date: August 9th, 2017
+# Purpose:  Lesson 2, Part 3 - GitHub
 
 
 # Import ArcPy
@@ -8,14 +8,17 @@ import arcpy
  
 
 # Set the workspace
- 
+arcpy.env.workspace = r"C:\Users\CRehak\Desktop\Hopkins\Programming in GIS\Module 2\Assignment2\Assignment2\Newark.gdb"
 
-# List all of the feature classes in the Newark geodatabase 
-
-
-# Iterate through all the feature classes and print the name of each feature class 
+# List all of the feature classes in the Newark geodatabase
+featureclass = arcpy.ListFeatureClasses()
 
 
+# Iterate through all the feature classes and print the name of each feature class
+for feature in featureclass:
+    print(feature)
+    
 # Print "Complete"
+print ("Complete")
 
 
