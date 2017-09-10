e539dc5874d0476923604d82f24b213ad3879b44
LotsaOcelots <LotsaOcelots@users.noreply.github.com>
2017-09-09 20:19:00 -0400

    Complete Feature List Script
    List features in the Newark.gdb file using a for loop and print the results.

--- ListFC.py
+++ ListFC.py
@@ -1,6 +1,6 @@
-# Author:  
-# Date:
-# Purpose:  
+# Author: Arin Franz
+# Date: 09/09/2017
+# Purpose: Listing Feature Classes
 
 
 # Import ArcPy
@@ -8,14 +8,14 @@ import arcpy
  
 
 # Set the workspace
- 
+arcpy.env.workspace = ("Z:\\Downloads\\Assignment2\\Newark.gdb")
 
-# List all of the feature classes in the Newark geodatabase 
-
+# List all of the feature classes in the Newark geodatabase
+listFC = arcpy.ListFeatureClasses()
 
 # Iterate through all the feature classes and print the name of each feature class 
-
+for fc in listFC:
+    print(fc)
 
 # Print "Complete"
-
-
+print("Complete")
