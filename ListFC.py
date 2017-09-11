ac08362e66d0391fe69f217130d3efe266da8e6b
ziminski13 <31832849+ziminski13@users.noreply.github.com>
2017-09-10 20:25:27 -0400

    Filled out code

--- ListFC.py
+++ ListFC.py
@@ -1,6 +1,6 @@
-# Author:  
-# Date:
-# Purpose:  
+# Author:  Andrew Ziminski
+# Date: 9/10/2017
+# Purpose:  assignment 2
 
 
 # Import ArcPy
@@ -9,13 +9,19 @@ import arcpy
 
 # Set the workspace
  
+arcpy.env.workspace = r"C:\Users\andre\Desktop\Programming\Assignment 2\Assignment2\Newark.gdb"
 
 # List all of the feature classes in the Newark geodatabase 
+featureclasses = arcpy.ListFeatureClasses()
 
+	
 
 # Iterate through all the feature classes and print the name of each feature class 
 
+for features in featureclasses:
+	print features
 
 # Print "Complete"
 
+print ("Complete")
 
